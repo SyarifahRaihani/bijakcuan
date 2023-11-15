0f0fdaf710f9dfc1517592e28f71503cf8747450
afrizalyogi <xxgdev@gmail.com>
2023-11-15 22:46:00 +0700

    Trial One by One

--- api/server.cjs
+++ api/server.cjs
@@ -97,5 +97,5 @@ app.post("/api/masuk", async (req, res) => {
 })
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`)
+	console.log(`Server is running on PORT ${PORT}`)
 })
