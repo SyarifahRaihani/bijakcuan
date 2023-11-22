96cbb46c879ac5cb93d17eeb180c014d42fc2be2
amida zulfa <amida.zulfa@upi.edu>
2023-11-22 16:06:54 +0700

    Add Profile, Kursus Saya, Transaksi Page

---
+++ src/pages/profile/KursusSaya.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+export default function KursusSaya() {
+  return (
+    <main id="kursusSaya">
+      <div className="card container">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-md-12 my-3">
+              <h5>Selamat Belajar</h5>
+            </div>
+            <div className="col-md-12">
+              <p>
+                Silahkan mempelajari materi kelas yang telah kami desain dengan
+                baik untuk mencapai goals Anda
+              </p>
+            </div>
+            <div className="col-md-12 mt-4">
+              <Link to={"/kursus"} className="btn btn-primary btn-sm">
+                  Mulai Belajar
+        </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
