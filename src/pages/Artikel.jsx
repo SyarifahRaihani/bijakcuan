8f1380235f7d138bbdde13e517bec59e7940e84d
amida zulfa <amida.zulfa@upi.edu>
2023-12-10 16:32:53 +0700

    Update Artikel Connect DB, Add Carousell in Hero

--- src/pages/Artikel.jsx
+++ src/pages/Artikel.jsx
@@ -1,57 +1,96 @@
 import "./css/artikel.css"
 import { Link } from "react-router-dom"
 import { Helmet } from "react-helmet"
-import artikelData from "../data/artikelData.json"
+import { useState, useEffect } from "react";
+import { SITE_URL } from "../utils/env"
+import Cta from "../components/cta"
+import axios from "axios";
 
 export default function Artikel() {
+	const getArtikel = async () => {
+		const response = await axios.get(`${SITE_URL}/api/artikel`)
+		setArtikel(response.data);
+	}
+	
+		const [artikel, setArtikel] = useState([]);
+	
+		useEffect(() => {
+			getArtikel();
+		}, []);
+
+		
+
 	return (
 		<main id="artikel">
 			<Helmet>
 				<title>Artikel | Bijakcuan.</title>
 			</Helmet>
 
-			<div className="hero">
-				<div className="container">
-					<div className="row justify-content-between flex-column-reverse flex-lg-row gap-5 gap-lg-0">
-						<div className="col-lg-6">
-							<h1 className="mb-4 text-white">
-								Kiat Meningkatkan Pendapatan Bisnis Tanpa Menambah Utang
-							</h1>
-							<p
-								className="deskripsi mb-5 text-white"
-								style={{ maxWidth: "600px" }}>
-								Pendapatan adalah salah satu aspek kunci dalam kesuksesan
-								bisnis. Meskipun seringkali pemilik bisnis menghadapi kebutuhan
-								untuk mendapatkan pendanaan tambahan...
-							</p>
-							<Link className="btn btn-primary" to={`/detail/5`}>
+			<div id="articleCarousel" className="carousel slide" data-bs-ride="carousel">
+				<div className="carousel-inner">
+					{artikel.map((article, index) => (
+					<div key={article.id} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+						<div className="hero">
+						<div className="container">
+							<div className="row justify-content-between flex-column-reverse flex-lg-row gap-5 gap-lg-0">
+							<div className="deskripsi col-lg-6">
+								<h1 className="mb-4 text-white">{article.judul}</h1>
+								<p className="deskripsi mb-5 text-white" style={{ maxWidth: '600px' }}>
+								{article.deskripsi}
+								</p>
+								<a href={`/detail/${article.id}`} className="btn btn-primary">
 								Lanjut Baca
-							</Link>
-						</div>
-						<div className="col-lg-6">
-							<img
+								</a>
+							</div>
+							<div className="col-lg-6">
+								<img
 								style={{
-									borderRadius: "25px",
-									width: "630px",
-									height: "300px",
+									borderRadius: '25px',
+									width: '630px',
+									height: '300px',
 								}}
 								className="img-fluid"
-								src="/assets/artikel/hero.jpg"
-								alt="Artikel Hero"
-							/>
+								src={article.image}
+								alt={`Artikel ${article.id}`}
+								/>
+							</div>
+							</div>
+						</div>
 						</div>
 					</div>
+					))}
 				</div>
+				<button
+					className="carousel-control-prev"
+					type="button"
+					data-bs-target="#articleCarousel"
+					data-bs-slide="prev"
+					style={{ left: '-70px' }} 
+				>
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Previous</span>
+				</button>
+				<button
+					className="carousel-control-next"
+					type="button"
+					data-bs-target="#articleCarousel"
+					data-bs-slide="next"
+					style={{ right: '-70px' }}
+				>
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Next</span>
+				</button>
 			</div>
+
 
 			<div className="container pt-5 pb-5">
 				<h2 className="mb-4">Artikel Terbaru</h2>
 				<div className="row">
-					{artikelData.map((artikel) => (
+					{artikel.map((artikel) => (
 						<div className="col-lg-3 col-md-6 mb-4" key={artikel.id}>
 							<div className="card">
 								<img
-									src={artikel.gambar}
+									src={artikel.image}
 									className="card-img-top"
 									alt={`Artikel ${artikel.id}`}
 								/>
@@ -71,6 +110,7 @@ export default function Artikel() {
 					))}
 				</div>
 			</div>
+			<Cta />
 		</main>
 	)
 }
