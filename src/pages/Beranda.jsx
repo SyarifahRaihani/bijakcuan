f19c01ede876ed43d4c10ade397d75f0c42e8493
afrizalyogi <xxgdev@gmail.com>
2023-12-11 20:15:21 +0700

    Update Validation Order

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -29,7 +29,12 @@ async function OrderValidate(order_id, status_code, transaction_status) {
 
 	const response = await axios.post(
 		`${import.meta.env.VITE_API_URL}/api/v1/orderValidation`,
-		data
+		data, {
+			withCredentials: true,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
 	)
 
 	if (response.status === 200) {
