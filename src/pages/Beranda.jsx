a992a06600b4889477d8c706cca509f7f8be3c19
afrizalyogi <xxgdev@gmail.com>
2023-12-11 20:16:30 +0700

    Fix Typo

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -29,12 +29,13 @@ async function OrderValidate(order_id, status_code, transaction_status) {
 
 	const response = await axios.post(
 		`${import.meta.env.VITE_API_URL}/api/v1/orderValidation`,
-		data, {
+		data,
+		{
 			withCredentials: true,
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
-		})
+		}
 	)
 
 	if (response.status === 200) {
