9b54e942759038c305d40e8c4f93e15ea820cb3f
Anandita Mulyadi <ananditakusumahm@upi.edu>
2023-12-17 21:58:45 +0700

    Edit Profile Backend

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -98,7 +98,19 @@ async function masuk(req, res) {
 					secretKey,
 					{ expiresIn: "1h" }
 				)
-				res.json({ token })
+
+				const isCourse = await query(
+					`SELECT status_order, id FROM orders WHERE user_id = ?;`,
+					[isUser[0].id]
+				)
+
+				if (isCourse.length > 0) {
+					if (isCourse[isCourse.length - 1].status_order == "settlement") {
+						res.json({ token: token, order: isCourse[isCourse.length - 1].id })
+					}
+				} else {
+					res.json({ token })
+				}
 			} else {
 				res.status(200).json({
 					wrongPass: "Password yang Anda masukan salah",
@@ -110,7 +122,110 @@ async function masuk(req, res) {
 	}
 }
 
+async function edit(req, res) {
+	const { username, name, email, phone, user } = await req.body
+	
+	if (
+		username === undefined ||
+		name === undefined ||
+		email === undefined ||
+		phone === undefined ||
+		user === undefined
+	) {
+		return res.status(400).json({ failed: "Data tidak lengkap." })
+	}
+
+	try {
+		const isUserExist = await query(`SELECT * FROM users WHERE username = ?;`, [
+			(username === user.username ? "NULL" : username),
+		])
+
+		const isEmailExist = await query(`SELECT * FROM users WHERE email = ?;`, [
+			(email === user.email ? "NULL" : email),
+		])
+
+		if (isUserExist.length > 0 && isEmailExist.length > 0) {
+			return res.status(200).json({
+				failed: "Username dan Email sudah digunakan",
+			})
+		} else if (isUserExist.length > 0) {
+			return res.status(200).json({
+				failed: "Username sudah digunakan",
+			})
+		} else if (isEmailExist.length > 0) {
+			return res.status(200).json({
+				failed: "Email sudah digunakan",
+			})
+		} else {
+			await query(
+				`
+				UPDATE users
+				SET name = ?, username = ?, email = ?, phone = ?
+				WHERE id = ?;`,
+				[name, username, email, phone, user.id]
+			)
+			return res.status(200).json({
+				success: "Data akun berhasil diubah.",
+			})
+		}
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+async function refreshToken(req, res)
+{
+	const { id } = await req.body
+
+	if (id === undefined) {
+		return res.status(400).json({ failed: "Data tidak lengkap." })
+	}
+
+	try {
+		const user = await query(
+			`SELECT * FROM users WHERE id = ?;`,
+			[id]
+		)
+
+		if (user.length === 0) {
+			return res.status(200).json({
+				wrongUser: "ID tidak terdaftar",
+			})
+		} else {
+			const token = jwt.sign(
+				{
+					id: user[0].id,
+					username: user[0].username,
+					name: user[0].name,
+					email: user[0].email,
+					phone: user[0].phone,
+					image: user[0]?.image,
+				},
+				secretKey,
+				{ expiresIn: "1h" }
+			)
+
+			const isCourse = await query(
+				`SELECT status_order, id FROM orders WHERE user_id = ?;`,
+				[user[0].id]
+			)
+
+			if (isCourse.length > 0) {
+				if (isCourse[isCourse.length - 1].status_order == "settlement") {
+					res.json({ token: token, order: isCourse[isCourse.length - 1].id })
+				}
+			} else {
+				res.json({ token })
+			}
+		}
+	} catch (err) {
+		console.log(err)
+	}
+}
+
 module.exports = {
 	daftar,
 	masuk,
+	edit,
+	refreshToken,
 }
--- src/pages/profile/Pengaturan.jsx
+++ src/pages/profile/Pengaturan.jsx
@@ -1,83 +1,155 @@
+import { jwtDecode } from "jwt-decode"
+import { Cookies } from "react-cookie"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom/dist"
+import axios from "axios"
 
 export default function Pengaturan() {
-  return (
-    <section id="pengaturan">
-      <div className="card" style={{ borderRadius: '10px' }}>
-        <div className="card-body">
-          <img
-            src="assets/profil/user.png"
-            alt="avatar"
-            className="rounded-circle img-fluid mt-3 mx-4"
-            style={{ width: '70px' }}
-          />
-          <button
-            type="button"
-            className="btn btn-outline-dark btn-sm mt-3"
-            style={{ borderRadius: '10px' }}
-          >
-            Upload Foto
-          </button>
-          <div className="row mt-4 mx-3">
-            <div className="col-md-12 my-3">
-              <label className="labels">Nama (Maks. 50 karakter)</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Masukkan Nama"
-                value="Ani"
-                style={{ backgroundColor: '#DCDCDC' }}
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Alamat Email</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Masukkan Alamat Email"
-                value="Ani.food@gmail.com"
-                style={{ backgroundColor: '#DCDCDC' }}
-              />
-            </div>
-            <div className="col-md-12 my-3">
-              <label className="labels">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Masukkan Username"
-                value="Ani.food"
-                style={{ backgroundColor: '#DCDCDC' }}
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">No Telepon</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Masukkan No Telepon"
-                value="08129834765"
-                style={{ backgroundColor: '#DCDCDC' }}
-              />
-            </div>
-            <div className="col-md-12 my-3">
-              <label className="labels">Domisili</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Masukkan Domisili"
-                value="Bandung"
-                style={{ backgroundColor: '#DCDCDC' }}
-              />
-            </div>
-            <button
-              type="button"
-              className="btn btn-primary btn-lg btn-block btn-sm mx-3"
-              style={{ width: '96%' }}
-            >
-              Simpan Profil Anda
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	const cookies = new Cookies()
+	const navigate = useNavigate()
+	const user = jwtDecode(cookies.get("auth-login"))
+
+	const [values, setValues] = useState({
+		username: user.username,
+		name: user.name,
+		email: user.email,
+		phone: user.phone,
+		user: user
+	})
+
+	const [failed, setFailed] = useState("")
+
+	const [isFormChanged, setIsFormChanged] = useState(false);
+
+	const handleInput = (event) => {
+		setValues((prev) => ({
+			...prev,
+			[event.target.name]: event.target.value,
+		}))
+
+		setIsFormChanged(true);
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		setFailed("")
+		if (isFormChanged) {
+			axios
+				.post(`${import.meta.env.VITE_API_URL}/api/v1/edit/`, values)
+				.then((res) => {
+					if (res.data.failed) {
+						const failed = res.data.failed
+						setFailed(failed)
+						return
+					} else {
+						cookies.remove("auth-login", { path: "/" })
+						cookies.remove("auth-order", { path: "/" })
+	
+						const refreshValue = {
+							id: user.id
+						}
+	
+						axios
+						.post(`${import.meta.env.VITE_API_URL}/api/v1/refreshToken/`, refreshValue)
+						.then((res) => {
+							const token = res.data.token
+							cookies.set("auth-login", token, { secure: true })
+							if (res.data.order) {
+								const order = res.data.order
+								cookies.set("auth-order", order, { secure: true })
+							}
+	
+							navigate("/profil")
+							window.location.reload()
+						})
+						.catch((error) => {
+							console.error("Error:", error)
+						})
+					}
+				})
+				.catch((error) => {
+					console.error("Error:", error)
+				})
+		} else {
+			console.log("No changes made.");
+		}
+	}
+
+	return (
+		<section id="pengaturan">
+			<div className="card" style={{ borderRadius: "10px" }}>
+				<div className="card-body">
+					{failed && (
+						<div id="wrong" className="alert alert-danger mb-4">
+							<p>{failed}</p>
+						</div>
+					)}
+					<img
+						src="assets/profil/user.png"
+						alt="avatar"
+						className="rounded-circle img-fluid mt-3 mx-4"
+						style={{ width: "70px" }}
+					/>
+					<button
+						type="button"
+						className="btn btn-outline-dark btn-sm mt-3"
+						style={{ borderRadius: "10px" }}>
+						Upload Foto
+					</button>
+					<div className="row mt-4 mx-3">
+						<div className="col-md-12 my-3">
+							<form onSubmit={handleSubmit}>
+								<label className="labels">Nama (Maks. 50 karakter)</label>
+								<input
+									onChange={handleInput}
+									type="text"
+									name="name"
+									id="name"
+									className="form-control mb-3"
+									defaultValue={user.name}
+									style={{ backgroundColor: "#DCDCDC" }}
+								/>
+								<label className="labels">Alamat Email</label>
+								<input
+									onChange={handleInput}
+									type="text"
+									name="email"
+									id="email"
+									className="form-control mb-3"
+									defaultValue={user.email}
+									style={{ backgroundColor: "#DCDCDC" }}
+								/>
+								<label className="labels">Username</label>
+								<input
+									onChange={handleInput}
+									type="text"
+									name="username"
+									id="username"
+									className="form-control mb-3"
+									defaultValue={user.username}
+									style={{ backgroundColor: "#DCDCDC" }}
+								/>
+								<label className="labels">No Telepon</label>
+								<input
+									onChange={handleInput}
+									type="text"
+									name="phone"
+									id="phone"
+									className="form-control mb-3"
+									defaultValue={user.phone}
+									style={{ backgroundColor: "#DCDCDC" }}
+								/>
+								<button
+									type="submit"
+									disabled={!isFormChanged}
+									className="btn btn-primary mt-3 w-100">
+									Simpan Perubahan
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
